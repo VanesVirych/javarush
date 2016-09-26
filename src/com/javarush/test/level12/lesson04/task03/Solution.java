8a47057bd59188895182fed72d4b98ec6c7d3935
vanie <ivirych@gmail.com>
2016-09-26 21:12:53 +0300

    Add solution 12_04, task 1 to 5

---
+++ src/com/javarush/test/level12/lesson04/task03/Solution.java
@@ -0,0 +1,20 @@
+package com.javarush.test.level12.lesson04.task03;
+
+/* Пять методов print с разными параметрами
+Написать пять методов print с разными параметрами.
+*/
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+
+    }
+
+    //Напишите тут ваши методы
+    public void print(int i) {}
+    public void print(Integer i) {}
+    public void print(String s) {}
+    public void print(long l) {}
+    public void print(double d) {}
+}
